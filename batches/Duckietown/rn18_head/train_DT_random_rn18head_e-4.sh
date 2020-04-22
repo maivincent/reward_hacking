8c7a9153ab957ef81102eb14d25f192b9e1c116d
Vincent Mai <vincent.mai@umontreal.ca>
2020-04-22 17:16:28 -0400

    Batch files correction

--- batches/Duckietown/rn18_head/train_DT_random_rn18head_e-4.sh
+++ batches/Duckietown/rn18_head/train_DT_random_rn18head_e-4.sh
@@ -3,4 +3,4 @@ cd ../../../src
 source activate dense_duckie
 
 python -u train_model.py -c mila -l Reward -e duckietown -g random -n DT_rn18_head_e-4 -m dk_resnet18_DT -r 0.0001
-python -u test_model.py -c mila -l Reward -e duckietown -g random -n DT_rn18_reg_e-4 -m dk_resnet18_DT
+python -u test_model.py -c mila -l Reward -e duckietown -g random -n DT_rn18_head_e-4 -m dk_resnet18_DT
