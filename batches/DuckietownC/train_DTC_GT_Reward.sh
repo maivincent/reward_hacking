8639e915a4593c0b2732a81c68a4acd822542a12
MaiVincent <vincent.mai@umontreal.ca>
2020-06-05 17:42:33 -0400

    Correction

--- batches/DuckietownC/train_DTC_GT_Reward.sh
+++ batches/DuckietownC/train_DTC_GT_Reward.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ../../../src
+cd ../../src
 source activate dense_duckie
 
 xvfb-run -a -s "-screen 0 1400x900x24" python -u rl_solver_sac.py -c local -e duckietown_cam -t DTC_GT_Reward
